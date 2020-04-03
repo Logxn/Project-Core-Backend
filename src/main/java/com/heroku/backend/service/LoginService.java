2e33b05c0beb1bda2b04e5195857734762c2ed89
Logan <hello@loganthompson.de>
2020-04-03 21:57:27 +0200

    gaben pls fix

--- src/main/java/com/heroku/backend/service/LoginService.java
+++ src/main/java/com/heroku/backend/service/LoginService.java
@@ -14,20 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Base64;
 
-import static javax.crypto.Cipher.*;
 
 @Service
 public class LoginService {
@@ -35,11 +24,6 @@ public class LoginService {
     private EmailRepository emailRepository;
     private UsersRepository usersRepository;
     private CryptoHelper cryptoHelper;
-
-    @Value("${encryption.salt}")
-    private String salt;
-    private static SecretKeySpec secretKey;
-    private static byte[] key;
 
     public LoginService(EmailRepository emailRepository, UsersRepository usersRepository){
         this.emailRepository = emailRepository;
