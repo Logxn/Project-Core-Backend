eb27720fbacc27c4d0183f3ea0cb6c22eafba345
Logan <hello@loganthompson.de>
2020-04-03 15:38:38 +0200

    oops

--- src/main/java/com/heroku/backend/SecurityConfiguration.java
+++ src/main/java/com/heroku/backend/SecurityConfiguration.java
@@ -4,16 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Value("${basic_auth.username}")
     private String username;
     @Value("${basic_auth.password}")
     private String password;
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/register", "/user/checkEmail").authenticated()
