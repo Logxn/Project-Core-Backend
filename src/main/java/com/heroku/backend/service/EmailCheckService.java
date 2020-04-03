67cbbb0e3104711566277be644b0b5c366b27827
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-04-03 08:21:40 +0200

    Implemented behaviour of EmailCheckService.java
    No further comments needed

--- src/main/java/com/heroku/backend/service/EmailCheckService.java
+++ src/main/java/com/heroku/backend/service/EmailCheckService.java
@@ -1,59 +1,33 @@
 package com.heroku.backend.service;
 
+import com.heroku.backend.data.ResponseData;
 import com.heroku.backend.entity.EmailEntity;
+import com.heroku.backend.enums.Status;
+import com.heroku.backend.repository.EmailRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
-import javax.validation.constraints.Email;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
+@Service
 public class EmailCheckService {
-    public ResponseEntity<Map<String, Object>> checkEmail(String email) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        LocalDateTime localDateTime = LocalDateTime.now();
-        Map<String, Object> response = new LinkedHashMap<>();
 
-        /*
-            Basic Idea:
-            When registering a company/user account - the frontend will ask for an email first
-            The frontend will then send a request to /user/checkEmail to see the current Status of this email.
+    public EmailRepository emailRepository;
 
-            If it exists in the database: it should come back with something like this
-            {"email":email, "status":"login", "timestamp":currentTimestamp}
+    public EmailCheckService(EmailRepository emailRepository) {
+        this.emailRepository = emailRepository;
+    }
 
-            If its not in the database: it should come back with something like this
-            {"email":email, "status":"register", "timestamp":currentTimestamp}
+    public ResponseEntity<ResponseData> checkEmail(String email) {
+        EmailEntity userFoundByEmail = emailRepository.findByEmail(email);
 
-            Either way - we will send back an OK response to the user.
-            The frontend will then decide how to continue with this email -> register or login.
+        ResponseData responseData = new ResponseData(email, LocalDateTime.now());
+        responseData.setStatus(Status.LOGIN);
 
-            No Authentication required.
+        if (userFoundByEmail == null) {
+            responseData.setStatus(Status.REGISTER);
+        }
 
-            Document idea:
-            {
-                "_id": {
-                    "$oid": "-snip-"
-                },
-                "email": "foo@loganthompson.de"
-            }
-
-         */
-
-        // Below is a fake object
-        // emailEntity will be a returned result from the database.
-        EmailEntity emailEntity = new EmailEntity(null);
-
-        response.put("email", email);
-        response.put("status", "login");
-
-        // Assuming the email wasn't found in the database
-        // We might get a null object (?)
-        if(emailEntity == null)
-            response.replace("status", "register");
-
-        response.put("timestamp", dateTimeFormatter.format(localDateTime));
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(responseData);
     }
 }
